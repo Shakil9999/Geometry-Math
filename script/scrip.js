429914499b746cca71ec5ccf3f5d6216c62eb7a3
Shakil <you@example.comshakilsabeer@gmail.com>
2024-10-28 16:04:47 +0600

    first commit

--- script/scrip.js
+++ script/scrip.js
@@ -1,3 +1,5 @@
+//                       traingle Area Calculate
+
 function calculateArea() {
   const tringlebaseinput = document.getElementById("traingle-base").value;
   const base = parseFloat(tringlebaseinput);
@@ -9,24 +11,91 @@ function calculateArea() {
 
   const taringleAreaSpan = document.getElementById("tringle-area");
   taringleAreaSpan.innerText = taringleArea;
-
 }
 
-function calculateRactangleArea(){
-    
 //                       Rectangle Area Calculate
 
-const  ractangleLength = document.getElementById('ractangler-length').value;
-const length=parseFloat(ractangleLength);
+function calculateRactangleArea() {
+  const ractangleLength = document.getElementById("ractangler-length").value;
+  const length = parseFloat(ractangleLength);
 
-const  ractanglewidth = document.getElementById('rectangler-width').value;
-const width=parseFloat(ractanglewidth);
+  const ractanglewidth = document.getElementById("rectangler-width").value;
+  const width = parseFloat(ractanglewidth);
 
-const ractangleArea =length*width
+  const ractangleArea = length * width;
 
-const ractangleAreaSpan=document.getElementById('ractangle-area');
-ractangleAreaSpan.innerText=ractangleArea;
+  const ractangleAreaSpan = document.getElementById("ractangle-area");
+  ractangleAreaSpan.innerText = ractangleArea;
+}
+
+//                       Rectangle Area parallelogram
+
+function calculateParallelogramArea() {
+  const parallelogramBase = document.getElementById("parallelogram-base").value;
+  const base = parseFloat(parallelogramBase);
+
+  const parallelogramHeight = document.getElementById(
+    "parallelogram-height"
+  ).value;
+  const height = parseFloat(parallelogramHeight);
+
+  const area = base * height;
+
+  const parallelogramArea = document.getElementById("parallelogram-area");
+  parallelogramArea.innerText = area;
+}
+
+
+//                     rhombus Area parallelogram
+
+function calculateRhombusArea(){
+
+const d1=getInputFromInputField('rhombus-d1');
+const d2 =getInputFromInputField('rhombus-d2')
+const area=0.5*d1*d2
+
+areaCalculator('rhombusArea-span',area)
 
 }
+
+//                     rhombus Area parallelogram
+
+function calculatePentagonArea(){
+  const p= getInputFromInputField('pentagon-p')
+  console.log(p)
+  const b= getInputFromInputField('pentagon-b')
+  const area= 0.5*p*b
+  areaCalculator('pentagonArea-span', area)
+
+}
+
+
+
+//                     Ellipse Area parallelogram
+
+function calculateEllipseArea(){
+  const a=getInputFromInputField('ellipse-a')
+  const b=getInputFromInputField('ellipse-b')
+  const math = Math;
+  const area=math.PI*a*b
+  areaCalculator('ellipsearea-span',area)
+}
+
+
+//              common function
+
+
+function getInputFromInputField(inputField){
+  const input =document.getElementById(inputField)
+  const inputValue =input.value;
+  return parseFloat(inputValue)
+}
+
+function areaCalculator(elementId , value){
+  const element=document.getElementById(elementId);
+  element.innerText=value
+
+}
+
 
 
